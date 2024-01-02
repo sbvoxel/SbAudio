c9f15fec78db7d0d365fa0493baf9a57e15c4791
orri <orri@systemb.is>
2024-01-02 12:50:51 +0000

    SbAudio: misc clarity

--- utils/facttool/facttool.cpp
+++ utils/facttool/facttool.cpp
@@ -29,7 +29,6 @@
 // Remove mentions of 'restrict' as we're including this project internal C header
 // into a C++ file.
 #define restrict
-
 #include <FACT_internal.h> /* DO NOT INCLUDE THIS IN REAL CODE! */
 
 #include "../uicommon/imgui.h"
